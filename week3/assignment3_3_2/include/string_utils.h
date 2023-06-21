d43cb7f51847f3b3d7d304d58674cf9d116ac9f7
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-06-21 15:38:56 +0300

    Exercise 2 done, string_utils + unit tests created

---
+++ week3/assignment3_3_2/include/string_utils.h
@@ -0,0 +1,11 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+#include <string>
+
+std::string reverse_string(std::string str);
+std::string to_lower_case(std::string str);
+bool is_palindrome(std::string str);
+
+
+#endif
--- week3/assignment3_3_2/tests/tests.cpp
+++ week3/assignment3_3_2/tests/tests.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "math_utils.h"
+#include "string_utils.h"
 
 #include <stdexcept>
 
@@ -17,4 +18,16 @@ TEST_CASE("Testing math_utils.cpp")
     CHECK(divide(15, 3) == 5);
     CHECK(divide(-15, 3) == -5);
     CHECK_THROWS_AS(divide(1, 0), std::exception);
+};
+
+TEST_CASE("Testing string_utils.cpp")
+{
+    CHECK(reverse_string("This should be") == "eb dluohs sihT");
+    CHECK(reverse_string("123, ABC, 321") == "123 ,CBA ,321");
+    CHECK(to_lower_case("THISSHOULDBE") == "thisshouldbe");
+    CHECK(to_lower_case("Checking f0r SP4ces! and IRREgul&rities")
+        == "checking f0r sp4ces! and irregul&rities");
+    CHECK(is_palindrome("Fish") == false);
+    CHECK(is_palindrome("ABBA") == true);
+    CHECK(is_palindrome("KaYAk") == true);
 };
