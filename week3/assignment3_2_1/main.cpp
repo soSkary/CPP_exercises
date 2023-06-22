f603cec964e96521623a5f5ff13080bdad3a9fc8
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-06-22 14:51:45 +0300

    Add function to go over all permutations of cities
    - Naive recursive function O(n!)

--- week3/assignment3_2_1/main.cpp
+++ week3/assignment3_2_1/main.cpp
@@ -4,37 +4,32 @@
 #include <string>
 #include <vector>
 
-void print_cities(std::vector<std::string> cities, std::vector<std::string> visited_cities)
+void print_cities(std::vector<std::string>& cities, std::size_t recursion)
 {
 
-    if (visited_cities.size() == cities.size()+1)
+    if (recursion == cities.size() -1)
     {
-        for (const auto& city : visited_cities)
+        for (const auto& city : cities)
         {
-            std::cout << city << ' ';
+            std::cout << city << "   ";
         }
         std::cout << '\n';
         return;
     }
-    for (int i{0}; i < cities.size();++i)
+    for (std::size_t i{recursion}; i < cities.size(); ++i)
     {
-        if (std::find(visited_cities.begin(), visited_cities.end(), cities.at(i)) != visited_cities.end())
-        {
-            continue;
-        }
-        else
-        {
-            visited_cities.push_back(cities.at(i));
-            print_cities(cities, visited_cities);
-            
-        }
+        //Swap places between cities
+        std::swap(cities.at(i), cities.at(recursion));
+        //Recursively go through the cities
+        print_cities(cities, recursion + 1);
+        //Swap again to return order of cities to original
+        std::swap(cities.at(i), cities.at(recursion));
     }
 }
 
 
 int main()
-{
-    std::vector<std::string> cities{"New York", "Amsterdam", "Helsinki", "Durban"};
-    std::vector<std::string> visited_cities{"Panama"};
-    print_cities(cities, visited_cities);
+{    
+    std::vector<std::string> cities{"Panama", "New York", "Amsterdam", "Helsinki", "Durban"};
+    print_cities(cities, 1);
 }
