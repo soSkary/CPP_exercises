7031344b7c55c8a6c045f31ec19d8bdb2c7d8b32
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-07-24 17:13:31 +0300

    Implement grid_2d
    -Otherwise functional, but no tests written

--- week7/assignment7_1_2/main.cpp
+++ week7/assignment7_1_2/main.cpp
@@ -1,67 +1,67 @@
-#include <iostream>
-#include <vector>
-
-namespace grid_2d {
-    
-    template <typename T>
-    class grid_2d
-    {
-    public:
-        
-        using value_type = typename T::value_type;
-        using reference = typename T::reference;
-        using const_reference = typename T::const_reference;
-        using iterator = std::vector<T>::iterator;
-        using const_iterator = std::vector<T>::const_iterator;
-        using difference_type = std::vector<T>::difference_type;
-        using size_type = std::vector<T>::size_type;
-        using storage_type = std::vector<T>;
-
-        std::size_t width{3};
-        std::size_t height{3};
-
-        std::size_t position;
-        T& at(std::size_t x, std::size_t y)
-        {
-            return data.at(x * width + y);
-        }
-
-        friend std::ostream& operator<<(std::ostream& out, const grid_2d& grid);
-
-    private:
-        std::vector<T> data{1,2,3,4,5,6,7,8,9};
-    };
-
-    // template <typename T>
-    // T& grid_2d::at(std::size_t x, std::size_t y)
-    // {
-    //     return data.at(x * width + y);
-    // }
-
-    std::ostream& operator<<(std::ostream& out, const grid_2d& grid)
-    {
-        for (int counter{ 0 }; const auto& element: grid.data)
-        {
-            out << element << ' ';
-            ++counter;
-
-            if (counter == width)
-            {
-                out << '\n';
-                counter = 0;
-            }
-        }
-        return out;
-    }
-}
-
+#include "grid_2d.hpp"
 
 
 int main()
 {
-    grid_2d::grid_2d grid{};
+    grid_2d::grid_2d<int> grid(5,5);
+    grid.fill_data();
 
+    std::cout << "front() " << grid.front() << " back() " << grid.back() << '\n';
+    std::cout << "Grid 5x5:\n";
     std::cout << grid;
+    std::cout << "size " << grid.size() << "\n\n";
+
+    grid_2d::grid_2d<int> grid2{grid};
+
+    std::cout << std::boolalpha << "grid == grid2: " << (grid == grid2) << "\n\n";
+    std::cout << std::boolalpha << "grid != grid2: " << (grid != grid2) << "\n\n";
+
+    grid_2d::grid_2d<int> a{ grid };
+    a.resize(7, 5);
+    std::cout << "Grid 7x5:\n";
+    std::cout << a;
+    std::cout << "size " << a.size() << "\n\n";
+    
+    std::cout << std::boolalpha << "grid == a: " << (grid == a) << "\n\n";
+    std::cout << std::boolalpha << "grid != a: " << (grid != a) << "\n\n";
+
+    grid_2d::grid_2d<int> b{ grid };
+    b.resize(5, 7);
+    std::cout << "Grid 5x7:\n";
+    std::cout << b;
+    std::cout << "size " << b.size() << "\n\n";
+    
+    grid_2d::grid_2d<int> c{ grid };
+    c.resize(3, 5);
+    std::cout << "Grid 3x5:\n";
+    std::cout << c;
+    std::cout << "size " << c.size() << "\n\n";
+    
+    grid_2d::grid_2d<int> d{ grid };
+    d.resize(5, 3);
+    std::cout << "Grid 5x3:\n";
+    std::cout << d;
+    std::cout << "size " << d.size() << "\n\n";
+    
+    grid_2d::grid_2d<int> e{ grid };
+    e.resize(7,7);
+    std::cout << "Grid 7x7:\n";
+    std::cout << e;
+    std::cout << "size " << e.size() << "\n\n";
+    
+    grid_2d::grid_2d<int> f{ grid };
+    f.resize(3, 3);
+    std::cout << "Grid 3x3:\n";
+    std::cout << f;
+    std::cout << "size " << f.size() << "\n\n";
+
+    std::cout << "f.max_size(): " << f.max_size() << '\n';
+
+    std::cout << "f.at(0,0) = 333\n";
+    f.at(0, 0) = 333;
+    std::cout << f;
+    std::cout << '\n';
+
 
     return 0;
 
