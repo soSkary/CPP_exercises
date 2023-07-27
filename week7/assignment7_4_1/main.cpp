9806254e72a9a460943f4b8560bf42807ae2abde
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-07-27 16:53:52 +0300

    Made changes
    - Made constructors to use common std::string name and sound
    - Had to make bird inherit virtual public animal -> don't really know
      what this does, so it should be re-examined, but otherwise parrot
      couldn't access animal constructor

--- week7/assignment7_4_1/main.cpp
+++ week7/assignment7_4_1/main.cpp
@@ -6,6 +6,7 @@
 class animal
 {
 public:
+    animal(const std::string& n, const std::string& s) : name{ n }, sound{ s } {}
     
     virtual std::string make_sound() const = 0;
     virtual int number_of_legs() const { return 0; }
@@ -13,59 +14,45 @@ public:
 
     virtual ~animal() = default;
 
-private:
     std::string name;
+    std::string sound;
 
 };
 
-class bird : public animal
+class bird : virtual public animal
 {
 public:
-    
+    bird() : animal{ "bird", "chirp-chirp" } {}
     std::string make_sound() const override { return sound; }
     int number_of_legs() const final { return 2; }
     bool breathes_air() const final { return true; }
     
     virtual ~bird() = default;
-
-private:
-    std::string name{ "bird" };
-    std::string sound{ "chirp-chirp" };
 };
 
 class parrot : public bird
 {
 public:
-    
+    parrot() : animal{ "parrot", "kwa-kwaa" } {}
     std::string make_sound() const final { return sound; }
 
-private:
-    std::string name{ "parrot" };
-    std::string sound{ "kwa-kwaa" };
 };
 
 class dog : public animal
 {
 public:
-    
+    dog() : animal{ "dog","woof-woof" } {}
     std::string make_sound() const final { return sound; }
     int number_of_legs() const final { return 4; }
     bool breathes_air() const { return true; }
-
-private:
-    std::string name{ "dog" };
-    std::string sound{ "woof-woof" };
 };
 
 class fish : public animal
 {
 public:
+    fish() : animal{ "fish", "blub-blub" } {}
     std::string make_sound() const final { return sound; }
     bool breathes_air() const final { return false; }
-
-private:
-    std::string name{ "fish" };
-    std::string sound{ "blub-blub" };
 };
 
 //Exercise 3
@@ -76,6 +63,7 @@ void dance_animal_dance(const animal& a)
     std::cout << "breathe air.\n";
     std::cout << a.make_sound();
     std::cout << "\nWhat am I?\n\n";
+    std::cout << "A " << a.name << "\n\n";
 }
 
 int main()
@@ -92,25 +80,27 @@ int main()
     dance_animal_dance(f);
 
 
-    //Compiler doesn't let me create a vector of different animals.
     
     std::cout << "\nEXERCISE 5\n\nCreating a vector of animals, "
         "and calling dance_animal_dance() for all animals in vector\n\n";
 
-    // std::vector<animal> gang;
-    // gang.push_back(b);
+    // Compiler doesn't let me create a vector of different animals, as the base 
+    // class has one pure virtual function
+    // virtual std::string make_sound() const = 0;
+
+    // std::vector<animal> gang{ b,p,d,f };
 
     // for (const auto& individual : gang)
     // {
     //     dance_animal_dance(individual);
     // }
 
-    //So I'll have to do it some othe way. This is one such, but maybe not the
-    //best or safest. Create a vector of animal pointers, and pass those derefenced 
-    //to the function instead.
+    // So I'll have to do it some othe way. This is one such, but maybe not the
+    // best or safest. Create a vector of animal pointers, and pass those derefenced 
+    // to the function instead.
 
-    std::vector<const animal*> gang{ &b, &p, &d, &f };
-    for (const auto& ptr : gang)
+    std::vector<const animal*> ptr_gang{ &b, &p, &d, &f };
+    for (const auto& ptr : ptr_gang)
     {
         dance_animal_dance(*ptr);
     }
