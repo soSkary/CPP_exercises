fcac1593a920b403c513aa3ede25661aa5d5bb2f
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-08-14 14:15:33 +0300

    Change to C++20 standard of cooperative cancellation
    - Use <stop_token> std::stop_source and std::stop_token

--- week10/assignment10_1_2/main.cpp
+++ week10/assignment10_1_2/main.cpp
@@ -2,17 +2,15 @@
 #include <chrono>
 #include <iostream>
 #include <mutex>
+#include <stop_token>
 #include <string>
 #include <thread>
 #include <vector>
-
-std::atomic_bool keep_alive{ true };
 
 struct log
 {
     std::vector<std::string> data;
     std::mutex log_mutex;
-
 };
 
 //Function to print out the log file (thread safe, read-only)
@@ -25,7 +23,7 @@ void output_log_file(const log& log_file)
 }
 
 //a function to read user input and log it
-void read_user_input(log& log_file)
+void read_user_input(log& log_file, std::stop_source st_source)
 {
     std::string input_string;
     while (input_string != "quit")
@@ -38,17 +36,20 @@ void read_user_input(log& log_file)
         }
             log_file.data.push_back(input_string);
     }
-    keep_alive = false;
+
+    st_source.request_stop();
 }
 
 //function to be run on a separate thread to log the time passing
-void log_message_every_second(log& log_file)
+void log_message_every_second(log& log_file, std::stop_source st_source)
 {
     using namespace std::chrono_literals;
-    
+
+    std::stop_token st_token{ st_source.get_token() };
+
     int elapsed{ 0 };
     std::string text{ "Seconds elapsed: "};
-    while (keep_alive)
+    while (!st_token.stop_requested())
     {
         std::this_thread::sleep_for(1s);
         ++elapsed;
@@ -60,10 +61,12 @@ void log_message_every_second(log& log_file)
 
 int main()
 {
+    std::stop_source st_source;
+    
     log log_file;
     {
-        std::jthread thread1{ read_user_input, std::ref(log_file) };
-        std::jthread thread2{ log_message_every_second, std::ref(log_file) };
+        std::jthread thread1{ read_user_input, std::ref(log_file), st_source };
+        std::jthread thread2{ log_message_every_second, std::ref(log_file), st_source };
     }
 
     return 0;
